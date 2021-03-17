c51506b4d14b3579bf635a58abdc0aa0c4dbd37e
Eryk Matecki <249484@student.pwr.edu.pl>
2021-03-17 10:02:39 +0100

    Problem plecakowy

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using Zadanie1;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()               //test sprawdzający generator liczb losowych
+        {
+            RandomNumberGenerator rng = new RandomNumberGenerator(1);
+            int x = rng.nextInt(1, 29);
+            Assert.IsTrue(x >= 1 && x <= 29);
+        }
+
+        [TestMethod]
+        public void TestMethod2()               //sprawdzenie czy jeśli co najmniej jeden element spełnia ograniczenia, to zwrócono co najmniej jeden element
+        {
+
+            RandomNumberGenerator rng = new RandomNumberGenerator(1);
+            List<int> weights = new List<int>();
+            List<int> index = new List<int>();
+            List<int> values = new List<int>();
+            for (int i = 0; i < 1; i++)
+                index.Add(i);
+            for (int j = 0; j < 1; j++)
+                values.Add(rng.nextInt(1, 29));
+            for (int k = 0; k < 1; k++)
+                weights.Add(5);
+            var b1 = new Program.Backpack(50);
+            b1.Load(index, values, weights);
+            int result = b1.Load_weights;
+            Assert.IsTrue(result >= 1);
+        }
+
+
+
+        [TestMethod]
+        public void TestMethod3()               //sprawdzenie czy jeśli żaden element nie spełnia ograniczeń, to zwrócono puste rozwiązanie
+        {
+
+            RandomNumberGenerator rng = new RandomNumberGenerator(1);
+            List<int> weights = new List<int>();
+            List<int> index = new List<int>();
+            List<int> values = new List<int>();
+            for (int i = 0; i < 1; i++)
+                index.Add(i);
+            for (int j = 0; j < 1; j++)
+                values.Add(rng.nextInt(1, 29));
+            for (int k = 0; k < 1; k++)
+                weights.Add(52);
+            var b1 = new Program.Backpack(50);
+            b1.Load(index, values, weights);
+            int result = b1.Load_weights;
+            Assert.IsTrue(result == 0);
+        }
+
+
+        [TestMethod]
+        public void TestMethod4()               //dwa testy sprawdzające czy kolejność przedmiotów ma wpływ na znalezione rozwiązanie
+        {
+
+            RandomNumberGenerator rng = new RandomNumberGenerator(1);
+            List<int> weights = new List<int>();
+            List<int> index = new List<int>();
+            List<int> values = new List<int>();
+            for (int i = 0; i < 4; i++)
+                index.Add(i);
+                values.Add(3);
+                values.Add(10);
+                values.Add(4);
+                values.Add(2);
+                weights.Add(30);
+                weights.Add(25);
+                weights.Add(10);
+                weights.Add(4);
+            var b1 = new Program.Backpack(50);
+            b1.Load(index, values, weights);
+            Assert.IsTrue(b1.Load_value == 9);
+        }
+
+
+
+        [TestMethod]
+        public void TestMethod5()               //dwa testy sprawdzające czy kolejność przedmiotów ma wpływ na znalezione rozwiązanie
+        {
+
+            RandomNumberGenerator rng = new RandomNumberGenerator(1);
+            List<int> weights = new List<int>();
+            List<int> index = new List<int>();
+            List<int> values = new List<int>();
+            for (int i = 0; i < 4; i++)
+                index.Add(i);
+            
+            values.Add(10);
+            values.Add(4);
+            values.Add(2);
+            values.Add(3);
+            weights.Add(25);
+            weights.Add(10);
+            weights.Add(4);
+            weights.Add(30);
+            var b1 = new Program.Backpack(50);
+            b1.Load(index, values, weights);
+            Assert.IsTrue(b1.Load_value == 16);
+        }
+
+    }
+}
---
+++ Zadanie1/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie1
+{
+    public class Program
+    {
+        public class Backpack
+        {
+            public Backpack(int capacity)
+            {
+                Capacity = capacity;
+            }
+            public int Capacity { get; }
+            public int Load_value = 0; 
+            public int Actual_load = 0; 
+            public int Load_weights = 0; 
+            public void Load(List<int> index, List<int> values, List<int> weights)      // funkcja Load, w której sprawdzamy czy aktualna waga w plecaku plus nowa  
+            {                                                                           // waga jest mniejsza od maksymalnej pojemności plecaka, gdy nie to aktualną
+                                                                                        // wagę przypisujemy do wartości załadowwanej i tą wartość zwracamy
+                foreach (int x in index)
+                {
+                    
+                    if ((Actual_load += weights[x]) <= Capacity)
+                    {
+                        Load_value += values[x];
+                        
+                        System.Console.Write("\t");
+                       
+                        System.Console.Write("\n");
+                    }
+                    else
+                    {
+                        Actual_load -= weights[x];
+                    }
+                }
+                Load_weights = Actual_load;
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            //int Capacity = 50;
+            RandomNumberGenerator rng = new RandomNumberGenerator(1);
+            List<int> index = new List<int>();                              //tworzenie list
+            List<int> values = new List<int>();
+            List<int> weights = new List<int>();
+
+            Console.Write("Podaj pojemnosc plecaka: ");                     //podawanie pojemności plecaka
+            int Capacity = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < 10; i++)                                    //wypełnianie list elementami
+                index.Add(i);
+            for (int j = 0; j < 10; j++)
+                values.Add(rng.nextInt(1, 29));
+            for (int k = 0; k < 10; k++)
+                weights.Add(rng.nextInt(1, 29));
+
+            Console.Write("Index: \t \t");
+            foreach (int x in index)
+            {
+                System.Console.Write(x + " \t");
+            }
+            Console.WriteLine();
+
+            Console.Write("Wartosc: \t ");
+            foreach (int y in values)
+            {
+                System.Console.Write(y + "\t");
+            }
+            Console.WriteLine();
+
+            Console.Write("Waga: \t \t");
+            foreach (int z in weights)
+            {
+                System.Console.Write(z + "\t");
+            }
+            Console.WriteLine();
+            var b1 = new Backpack(Capacity);
+            Console.WriteLine();
+            Console.WriteLine("Wybrana pojemnosc: " + b1.Capacity);
+            b1.Load(index, values, weights);
+            Console.Write("Waga: ");
+            Console.WriteLine(b1.Load_weights);
+            Console.WriteLine();
+            Console.Write("Wartosc: ");
+            Console.WriteLine(b1.Load_value);
+            System.Console.ReadLine();
+        }
+    }
+
+}
